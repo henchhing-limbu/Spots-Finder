8e1dcfbf5cbc833ff8146db3195e6155bdd63b3c
Aayush Gupta <aayush.gupta@bison.howard.edu>
2019-04-11 13:22:24 -0700

    Completed basic hello world.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.get('/', (req, res) => {
     res
         .status(200)
-        .sendFile('html/request.html', {root: __dirname})
+        .send('hello world!')
         .end();
 });
 
